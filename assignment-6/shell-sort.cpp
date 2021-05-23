0f2adccb8a64e4077a16e74716ac2b2114b82501
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-05-23 16:09:46 +0530

    added shell sort

---
+++ assignment-6/shell-sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void shellSort(int *a, int n)
+{
+    int gap, i, j, temp;
+
+    for (gap = n / 2; gap >= 1; gap /= 2)
+    {
+        for (i = gap; i < n; i++)
+        {
+            temp = a[i];
+            j = i - gap;
+            while (j >= 0 && a[j] > temp)
+            {
+                a[j + gap] = a[j];
+                j = j - gap;
+            }
+            a[j + gap] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array" << endl;
+    cin >> n;
+    int *a = new int[n];
+    cout << "Enter the elements of array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << "The initial array is: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    shellSort(a, n);
+    cout << "The sorted array is: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
