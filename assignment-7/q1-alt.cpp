823cdd46f217a0e2878a6a0de9a1b43fdad86ae1
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-05-31 14:52:09 +0530

    added

---
+++ assignment-7/q1-alt.cpp
@@ -0,0 +1,173 @@
+// TODO Delete operation
+// Hashing using chaining
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+class HashTable
+{
+public:
+    Node **HT;
+    int size;
+
+    HashTable(int size)
+    {
+        this->size = size;
+        HT = new Node *[size];
+        for (int i = 0; i < size; i++)
+        {
+            HT[i] = NULL;
+        }
+    }
+
+    // * Generates the hashing index for the incoming value
+    int hash(int key)
+    {
+        return key % 10;
+    }
+
+    // * Performing sorted insert on the hashtable
+    void Insert(int key)
+    {
+        int hashIndex = hash(key);
+        Node *newNode = new Node;
+        newNode->data = key;
+        newNode->next = NULL;
+
+        if (HT[hashIndex] == NULL)
+        {
+            HT[hashIndex] = newNode;
+        }
+
+        else
+        {
+            Node *prev = HT[hashIndex];
+            Node *curr = HT[hashIndex];
+            while (curr && curr->data < key)
+            {
+                prev = curr;
+                curr = curr->next;
+            }
+
+            // * The element is found at the very first index
+            if (prev == HT[hashIndex])
+            {
+                newNode->next = HT[hashIndex];
+                HT[hashIndex] = newNode;
+            }
+
+            else
+            {
+                newNode->next = prev->next;
+                prev->next = newNode;
+            }
+        }
+    }
+
+    // * Searches for a element in the hash table
+    int Search(int value)
+    {
+        int hashIndex = hash(value);
+        Node *h = HT[hashIndex];
+        while (h)
+        {
+            if (h->data == value)
+            {
+                return h->data;
+            }
+            h = h->next;
+        }
+
+        return -1;
+    }
+
+    // * Displays the Hash Table
+    void Display()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            Node *n = HT[i];
+            while (n != NULL)
+            {
+                cout << n->data << "-->";
+                n = n->next;
+            }
+            cout << endl;
+        }
+    }
+
+    ~HashTable()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            Node *p = HT[i];
+            while (HT[i])
+            {
+                HT[i] = HT[i]->next;
+                delete p;
+                p = HT[i];
+            }
+        }
+        delete[] HT;
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Enter the size of HashTable" << endl;
+    cin >> n;
+    HashTable h(n);
+    int ch;
+    int ans = 1;
+    do
+    {
+        cout << "--Menu--" << endl;
+        cout << "1.Insert" << endl;
+        cout << "2.Search" << endl;
+        cout << "3.Display" << endl;
+        cout << "4.Exit" << endl;
+        cout << "Enter your choice: " << endl;
+        cin >> ch;
+
+        switch (ch)
+        {
+        case 1:
+            cout << "Enter the element to be inserted:" << endl;
+            int element;
+            cin >> element;
+            h.Insert(element);
+            break;
+        case 2:
+            cout << "Enter the element to be searched: " << endl;
+            int key;
+            cin >> key;
+            if (h.Search(key) != -1)
+            {
+
+                cout << "Found value: " << h.Search(key) << endl;
+            }
+            else
+            {
+                cout << "Could not find the given value" << endl;
+            }
+            break;
+        case 3:
+            h.Display();
+            cout << endl;
+            break;
+        case 4:
+            ans = 0;
+            break;
+        default:
+            cout << "Please enter a valid input" << endl;
+            break;
+        }
+    } while (ans == 1);
+}
